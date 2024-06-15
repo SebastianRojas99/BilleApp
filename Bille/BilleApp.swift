e058dbe16f004cb6935832103e7e0fc00b370958
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-06-14 23:14:04 -0300

    Initial Commit

---
+++ Bille/BilleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BilleApp.swift
+//  Bille
+//
+//  Created by Sebastian Marquez Rojas on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BilleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
