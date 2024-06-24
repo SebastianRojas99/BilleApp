18a451acd6825159968cf8f2f72e1a55942ba304
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-06-24 18:31:33 -0300

    servicesVM added.

---
+++ Bille/ViewModel/ServicesVM.swift
@@ -0,0 +1,29 @@
+//
+//  ServicesVM.swift
+//  Bille
+//
+//  Created by Sebastian Marquez Rojas on 24/06/2024.
+//
+
+import Foundation
+import SwiftUI
+import Observation
+
+@Observable
+class ServicesVM{
+    
+    
+    
+    func get(){
+        
+    }
+    func add(){
+        
+    }
+    func update(){
+        
+    }
+    func delete(){
+        
+    }
+}
