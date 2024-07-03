76887b52914998792bb1557fbf22e96e3fae3250
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-07-03 20:08:02 -0300

    balance view implemented homescreen changes in progress.

---
+++ Bille/View/BalanceView.swift
@@ -0,0 +1,18 @@
+//
+//  BalanceView.swift
+//  Bille
+//
+//  Created by Sebastian Marquez Rojas on 03/07/2024.
+//
+
+import SwiftUI
+
+struct BalanceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BalanceView()
+}
