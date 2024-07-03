260d72333d3634651e8fc83f99fbacb5d3f4a061
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-07-03 20:50:33 -0300

    navbar created navbar in progress.

---
+++ Bille/View/NavBarView.swift
@@ -0,0 +1,22 @@
+//
+//  NavBarView.swift
+//  Bille
+//
+//  Created by Sebastian Marquez Rojas on 03/07/2024.
+//
+
+import SwiftUI
+
+struct NavBarView: View {
+    var body: some View {
+        HStack{
+            ScrollView{
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    NavBarView()
+}
