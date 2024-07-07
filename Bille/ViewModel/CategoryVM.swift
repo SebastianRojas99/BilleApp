5c8b6769a724256b0021b2d97e59bce4fda2fb49
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-07-07 20:13:00 -0300

    category viewmodel added.

---
+++ Bille/ViewModel/CategoryVM.swift
@@ -0,0 +1,15 @@
+//
+//  CategoryVM.swift
+//  Bille
+//
+//  Created by Sebastian Marquez Rojas on 07/07/2024.
+//
+
+import Foundation
+import Observation
+
+
+@Observable
+class CategoryVM{
+    
+}
