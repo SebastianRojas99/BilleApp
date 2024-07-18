92fd90e914154ece907ec8e96a278a0bcd152a54
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-07-18 12:51:02 -0300

    hardcoded data users fixed.

--- Bille/Credentials/DatabaseHardcoded.swift
+++ Bille/Credentials/DatabaseHardcoded.swift
@@ -14,7 +14,7 @@ var users: [User] = [
          lastname: "Rojas",
          username: "seba", email: "sebarojas@example.com",
          address: "AV gatorade 123",
-         birthday: DateFormatter.yearMonthDay.date(from: "2014-06-06") ?? Date() ,
+         birthday: DateFormatter.yearMonthDay.date(from: "1999-06-06") ?? Date() ,
          password: "123",
          accountAmount: 300, cards: [
             Card(name: "S MARQUEZ ROJAS".lowercased(), typeCard: "MasterCard", number: "2550 3030 9494 1112", expiryDate: "11/28", cvv: "333", credit: 1000000),
@@ -26,7 +26,7 @@ var users: [User] = [
          lastname: "man",
          username: "apple-man", email: "apple-man@example.com",
          address: "AV manzana 999",
-         birthday: DateFormatter().date(from: "01/15/1995") ?? Date(),
+         birthday: DateFormatter.yearMonthDay.date(from: "1995-02-04") ?? Date() ,
          password: "123",
          accountAmount: 10000, cards: [
             Card(name: "APPLE-MAN".lowercased(), typeCard: "MasterCard", number: "2550 3030 9494 3232", expiryDate: "11/28", cvv: "333", credit: 1000000),
@@ -38,7 +38,7 @@ var users: [User] = [
          lastname: "S",
          username: "cande", email: "cande@example.com",
          address: "AV bb 290",
-         birthday: DateFormatter().date(from: "01/15/2001") ?? Date(),
+         birthday: DateFormatter.yearMonthDay.date(from: "2001-05-23") ?? Date() ,
          password: "123",
          accountAmount: 500, cards: [
             Card(name: "CANDELA S".lowercased(), typeCard: "MasterCard", number: "4411 6585 2234 1238", expiryDate: "11/28", cvv: "333", credit: 1000000),
