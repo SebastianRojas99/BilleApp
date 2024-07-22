2f67e73b62eefe176b6031214510594d6b7179aa
Sebastian <96402832+SebastianRojas99@users.noreply.github.com>
2024-07-22 10:44:01 -0300

    progress view in currency added.

--- Bille/View/CurrencyMenu.swift
+++ Bille/View/CurrencyMenu.swift
@@ -13,42 +13,47 @@ struct CurrencyMenu: View {
     var body: some View {
         NavigationStack {
             VStack {
-                List {
-                    ForEach(currencyVM.currencyList, id: \.self) { item in
-                        if item.type == "fiat" {
-                            VStack {
-                                HStack {
-                                    Text("\(item.unit)")
-                                        .font(.system(size: 14))
-                                        .padding(.all, 5)
-                                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                                        .frame(width: 35, height: 30)
-                                        .foregroundColor(.white)
-                                        .padding(.all, 10)
-                                        .background(Color.red.opacity(0.4))
-                                        .clipShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
-                                    
-                                    VStack(alignment: .leading) {
-                                        Text("\(item.name)")
+                if !currencyVM.currencyList.isEmpty{
+                    List {
+                        ForEach(currencyVM.currencyList, id: \.self) { item in
+                            if item.type == "fiat" {
+                                VStack {
+                                    HStack {
+                                        Text("\(item.unit)")
+                                            .font(.system(size: 14))
+                                            .padding(.all, 5)
+                                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                                            .frame(width: 35, height: 30)
+                                            .foregroundColor(.white)
+                                            .padding(.all, 10)
+                                            .background(Color.red.opacity(0.4))
+                                            .clipShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
+                                        
+                                        VStack(alignment: .leading) {
+                                            Text("\(item.name)")
+                                                .font(.caption)
+                                                .foregroundColor(.gray)
+                                        }
+                                        .padding()
+                                        
+                                        Spacer()
+                                        
+                                        Text(String(format: "%.2f", item.value / currencyVM.dolarValue()))
+                                            .bold()
                                             .font(.caption)
-                                            .foregroundColor(.gray)
                                     }
-                                    .padding()
-                                    
-                                    Spacer()
-                                    
-                                    Text(String(format: "%.2f", item.value / currencyVM.dolarValue()))
-                                        .bold()
-                                        .font(.caption)
+                                    .frame(maxWidth: .infinity) // Asegurarse de que el HStack ocupe todo el ancho
                                 }
-                                .frame(maxWidth: .infinity) // Asegurarse de que el HStack ocupe todo el ancho
+                                .padding(.vertical, 5) // Espacio entre las celdas de la lista
                             }
-                            .padding(.vertical, 5) // Espacio entre las celdas de la lista
                         }
                     }
+                    .listStyle(PlainListStyle()) // Estilo de lista plano
+                    .frame(maxWidth: .infinity) // Asegurarse de que la lista ocupe todo el ancho
+                }else{
+                    ProgressView()
                 }
-                .listStyle(PlainListStyle()) // Estilo de lista plano
-                .frame(maxWidth: .infinity) // Asegurarse de que la lista ocupe todo el ancho
+                
             }
             .preferredColorScheme(.dark)
             .frame(maxWidth: .infinity) // Asegurarse de que el VStack ocupe todo el ancho
